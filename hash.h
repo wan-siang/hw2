7174b22c8786a8f7d812b3529de89847e061a033
wan-siang <80023755+wan-siang@users.noreply.github.com>
2021-05-24 21:46:18 +0800

    Update hash.h

--- hash.h
+++ hash.h
@@ -1,4 +1,3 @@
-#include	"basic.h"
 #define		MAXHASH	1000003
 
 struct	dnode{
@@ -6,38 +5,6 @@ struct	dnode{
 	struct 	dnode	*next;
 };
 
-unsigned int hash65(char *key){
-	unsigned int	hv = 0;
-	char 	*ptr = key;
-
-	while(*ptr){
-		hv = hv * 65 + *ptr;
-		ptr++;
-	}
-	return	hv;	 
-}
-
-struct dnode *h_insert(struct dnode *list, char *key){
-	struct dnode *p;
-	p = (struct dnode *)malloc(sizeof(struct dnode));
-	p->key = strdup(key);
-	if(list == NULL){
-		p->next = NULL;
-		return p;
-	}
-	p->next = list;
-	return p;
-}
-
-struct dnode *h_find(struct dnode *list, char *key){
-	struct dnode *p = list;
-
-	while(p){
-		if(strcmp(p->key,key)==0){
-			return p;
-		}		
-		p = p -> next;
-	}
-	return NULL;
-}
-
+unsigned int hash65(char *key);
+struct dnode *h_insert(struct dnode *list, char *key);
+struct dnode *h_find(struct dnode *list, char *key);
