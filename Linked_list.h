bdcdfb98c35457e00bd73e87c7fecd1bd468c67b
wan-siang <80023755+wan-siang@users.noreply.github.com>
2021-05-25 17:35:39 +0800

    Update Linked_list.h

--- Linked_list.h
+++ Linked_list.h
@@ -1,41 +1,4 @@
-#include	"basic.h"
+struct	lnode	*Linsert(struct lnode *L, char *str);
+struct lnode	*Lfind(struct lnode *L, char *str);
 
-struct lnode {
-	char	*key;
-	struct lnode	*next;
-};
-
-struct	lnode	*Linsert(struct lnode *L, char *str)
-{
-	struct lnode *p = (struct lnode *)malloc(sizeof(struct lnode));
-	p->key = strdup(str);
-	p->next = L;
-	if(L==NULL){
-		p->next = NULL;
-		return p;
-	}	
-	
-	struct lnode *q = L;
-	while(q->next){
-		q = q->next;
-	}
-	q->next = p;
-	p->next = NULL;
-	return L;
-
-}
-
-struct lnode	*Lfind(struct lnode *L, char *str)
-{
-	struct lnode *p = L;
-
-	while(p)
-	{
-		if(strcmp(p->key,str)==0)
-		{
-			return p;
-		}
-		p=p->next;
-	}
-	return NULL;
-}
+#include	"Linked_list.c"
