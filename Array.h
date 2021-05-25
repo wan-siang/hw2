8051057fe8f1b97de8479203f96cb8682f7590de
wan-siang <80023755+wan-siang@users.noreply.github.com>
2021-05-25 16:58:51 +0800

    Update Array.h

--- Array.h
+++ Array.h
@@ -1,1 +1,10 @@
-int	arr_find(char *line[], char *str, int cnt);
+#include	"basic.h"
+
+int	arr_find(char *line[], char *str, int cnt){
+	for(int i = 0; i < cnt; i++){
+		if(strcmp(line[i],str) == 0){
+			return i;
+		}
+	}
+	return -1;
+}
